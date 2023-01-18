52c74595edd67c3832517cfb0f933d9b215560a8
felipelens <uruena271@gmail.com>
2023-01-18 18:09:10 -0500

    faltan las imagenes

--- php/magia.php
+++ php/magia.php
@@ -1,44 +1,35 @@
 <?php
-$sql = mysqli_connect("localhost","root","","lolcito");
+
+$eleccion = $_POST['btn'];
+echo $eleccion;
+
+
+$conx =include("conexion.php");
+
+    if ($conx) {
+        $consulta = "SELECT * from campeones WHERE id = $eleccion";
+        $resultado = mysqli_query($conex,$consulta); 
+        if ($resultado) {
+            while ($row = $resultado -> fetch_array()) {
+                $id = $row['id'];
+                $nombre = $row['nombre'];
+                $localidad = $row['localidad'];
+                $linea = $row['linea'];
+                 
+                ?>
+    <div>
+        <div>
+            <p>
+                <b>NOMBRE:</b><?php echo $nombre;?> <br>
+                <b>LOCALIDAD </b><?php echo $localidad; ?><br>
+                <b>LINEA </b> <?php echo $linea; ?>
+            </p>
+        </div>
+    </div>
+    <?php
+        }
+    }
+}
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>aca se mostrara lo prron</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <td>id</td>
-            <td>nombre</td>
-            <td>localidad</td>
-            <td>linea</td>
-        </tr>
-    <?php 
-
-    $consulta = "SELECT * from campeones";
-
-    $tabla = mysqli_query($sql,$consulta);
-
-    while($arreglo = mysqli_fetch_array($tabla)){
-    ?>
-
-    <tr>
-        <td><?php echo $mostrar['id'] ?></td>
-        <td><?php echo $mostrar['nombre'] ?></td>
-        <td><?php echo $mostrar['localidad'] ?></td>
-        <td><?php echo $mostrar['inea'] ?></td>
-    </tr>
-
-    <?php 
-    }
-    ?>
-    </table>
-</body>
-</html>
---
+++ php/prueba.php
@@ -0,0 +1,14 @@
+<?php
+
+$eleccion = $_POST['btn1'];
+echo $eleccion;
+
+$eleccion = $_POST['btn2'];
+echo $eleccion;
+
+$eleccion = $_POST['btn3'];
+
+echo $eleccion;
+
+
+?>
